7dfb4e081657c637fd1a635920b2c227996d256b
wayry <wayrygaming@gmail.com>
2022-03-17 16:04:50 +0100

    comptes

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('comptes.index');
+})->middleware(['auth'])->name('comptes');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
