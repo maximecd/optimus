305eb26b4be54a501995e3534769d78aee3fbdd5
maximecd <37560356+maximecd@users.noreply.github.com>
2022-03-17 09:29:24 +0100

    Revert "added breeze"
    This reverts commit 745dc5dfc99199d2016a96af54673191fa5d45fc.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nom');
-            $table->string('prenom');  
-            $table->string('photoprofil');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
