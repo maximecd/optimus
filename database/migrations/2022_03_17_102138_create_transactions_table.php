6ea0f8903178ca0ff38c4834563aca8170965362
wayry <wayrygaming@gmail.com>
2022-03-17 12:05:07 +0100

    blabla

--- database/migrations/2022_03_17_102138_create_transactions_table.php
+++ database/migrations/2022_03_17_102138_create_transactions_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -17,24 +16,32 @@ return new class extends Migration
             $table->id();
             $table->string('intitule');
             $table->string('description');
-            $table->Integer('montant');
-            $table->string('sens_transaction');
-            $table->dateTime('datetime');
+            $table->decimal('montant', 10, 2);
+            $table->enum('sens_transaction', ['entrant', 'sortant']);
 
             $table->unsignedBigInteger('compte_id')->nullable();
-            $table->foreign('compte_id')->references('id')
-                ->on('compte')
-                ->onDelete('restrict')->onUpdate('restrict');
+            $table
+                ->foreign('compte_id')
+                ->references('id')
+                ->on('comptes')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
 
             $table->unsignedBigInteger('user_id')->nullable();
-            $table->foreign('user_id')->references('id')
+            $table
+                ->foreign('user_id')
+                ->references('id')
                 ->on('users')
-                ->onDelete('restrict')->onUpdate('restrict');
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
 
             $table->unsignedBigInteger('categorie_id')->nullable();
-            $table->foreign('categorie_id')->references('id')
-                ->on('categorie')
-                ->onDelete('restrict')->onUpdate('restrict');
+            $table
+                ->foreign('categorie_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
 
             $table->timestamps();
         });
@@ -48,6 +55,6 @@ return new class extends Migration
     public function down()
     {
         Schema::dropIfExists('transactions');
-        Schema::disableForeignKeyConstraints(); 
+        Schema::disableForeignKeyConstraints();
     }
 };
