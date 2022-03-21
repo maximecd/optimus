006de6b67556330efdd121e02281f3af9a195797
Jérémy PION <77725625+jrmy17@users.noreply.github.com>
2022-03-21 15:57:48 +0100

    Create index.blade.php

---
+++ resources/views/transaction/index.blade.php
@@ -0,0 +1,37 @@
+<x-app-layout>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Intitule de la transaction</th>
+                    <th>Description</th>
+                    <th>Montant</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($transactions as $transaction)
+                    <tr>
+                        <td>{{ $transaction->id }}</td>
+                        <td><strong>{{ $transaction->intitule }}</strong></td>
+                        <td><strong>{{ $transaction->description }}</strong></td>
+                        <td><strong>{{ $transaction->montant }}</strong></td>
+                        <td><a href="{{ route('transaction.show', $transaction->id) }}">Voir</a></td>
+                        <td><a href="{{ route('transaction.edit', $transaction->id) }}">Modifier</a></td>
+                        <td>
+                            <form action="{{ route('transaction.destroy', $transaction->id) }}" method="post">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+                                <button type="submit">Supprimer</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
+</x-app-layout>
