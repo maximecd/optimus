1384b1be999430d6789fba0b28b78904df80daa1
wayry <wayrygaming@gmail.com>
2022-03-24 09:44:44 +0100

    compte

--- resources/views/compte/index.blade.php
+++ resources/views/compte/index.blade.php
@@ -3,5 +3,39 @@
 @section('title', 'Accueil')
 
 @section('content')
-    <p>Test</p>
+<p>Test</p>
+<table class="table is-hoverable">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Comptes</th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($comptes as $compte)
+        <tr>
+            <td>{{ $compte->id }}</td>
+            <td><strong>{{ $compte->intitule }}</strong></td>
+            <td><a class="button is-primary" href="{{ route('compte.show', $compte->id) }}">Voir</a></td>
+            <td><a class="button is-warning" href="{{ route('compte.edit', $compte->id) }}">Modifier</a></td>
+            <td>
+                <form action="{{ route('compte.destroy', $compte->id) }}" method="post">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
+                    <button class="button is-danger" type="submit">Supprimer</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+</div>
+<footer class="card-footer">
+
+</footer>
+</div>
 @endsection
