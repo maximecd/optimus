7999a826580af27f08dea521015c5a1480635763
wayry <wayrygaming@gmail.com>
2022-03-24 08:55:33 +0100

    azfd

--- routes/compte.php
+++ routes/compte.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    return view('compte.index');
-})->middleware(['auth'])->name('compte');
+    return view('compte/index');
+})->middleware(['auth'])->name('compte.index');
