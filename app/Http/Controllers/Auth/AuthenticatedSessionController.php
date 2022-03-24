06a2ae7e76d84c04ec4fd56883d83025e769c2b3
maximecd <37560356+maximecd@users.noreply.github.com>
2022-03-24 09:24:03 +0100

    redirect

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -32,7 +32,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::COMPTES);
+        return redirect()->intended(RouteServiceProvider::COMPTE);
     }
 
     /**
