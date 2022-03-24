b658a3014a9dcc4d93cc0b3cb623051bd3fbb453
Jérémy PION <77725625+jrmy17@users.noreply.github.com>
2022-03-24 08:51:45 +0100

    factory categorie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Transaction;
+use App\Models\Categorie;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,8 +19,9 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(50)->create();
         User::factory(1)->asAdmin()->create();
+        Categorie::factory(10)->create();
         //$ids = range(1, 10);
-        Transaction::factory(10)->create();
+        //Transaction::factory(10)->create();
         // Transaction::factory()->count(10)->create()->each(function ($transaction) use ($ids) {
         //     shuffle($ids);
         //     $transaction->id_compte()->attach(array_slice($ids, 0, rand(1, 1)));
