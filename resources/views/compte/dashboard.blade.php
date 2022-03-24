45b19c70ccbe7d48bf8ec40e803f1b09ed3154de
wayry <wayrygaming@gmail.com>
2022-03-24 11:39:28 +0100

    dashboard 2

--- resources/views/compte/dashboard.blade.php
+++ resources/views/compte/dashboard.blade.php
@@ -2,9 +2,29 @@
 
 @section('content')
 
-   <h1>Tableau de bord - {{ $compte->intitule }}</h1>
+<h1 class="head-dashboard">Tableau de bord - {{ $compte->intitule }}</h1>
 
-    <div>
+<div class="solde">
+    <p>Votre solde :</p>
+</div>
+
+<div class="historique-transaction">
+    @foreach ($transactions as $transaction)
+    <tr>
+        <td>{{ $transaction->id }}</td>
+        <td><strong>{{ $transaction->intitule }}</strong></td>
+        <td><strong>{{ $transaction->description }}</strong></td>
+        <td><strong>{{ $transaction->montant }}</strong></td>
+    </tr>
+@endforeach
+</div>
+
+<div class="depense-recette">
+    <div class="depense">
         
     </div>
+    <div class="recette">
+        
+    </div>
+</div>
 @endsection
