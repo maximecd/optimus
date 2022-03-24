0e9869cd59874d70152f6468595488b62e9cac18
wayry <wayrygaming@gmail.com>
2022-03-24 11:14:33 +0100

    dashboard

--- resources/views/compte/dashboard.blade.php
+++ resources/views/compte/dashboard.blade.php
@@ -1,20 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex flex-nowrap">
-        @foreach ($comptes as $compte)
-            <a href="#route-a-ajouter">
-                <div class="shadow rounded-xl w-[200px] h-[200px] bg-slate-50 p-3 flex flex-col justify-between">
-                    <p class="text-lg font-bold"> {{ $compte->intitule }}</p>
-                    <p class="text-cyan-600 self-end">Accéder ➟</p>
-                </div>
-            </a>
-        @endforeach
 
-        <a href="{{ route('compte.create') }}">
-            <div class="shadow rounded-xl w-[200px] h-[200px] bg-slate-50 p-3 flex justify-center items-center">
-                <p class="text-cyan-600 self-end">+</p>
-            </div>
-        </a>
+   <h1>Tableau de bord - {{{{ $compte->intitule }}}}</h1>
+
+    <div>
+        
     </div>
 @endsection
