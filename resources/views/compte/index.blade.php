c752a2dbee2fa83a00052fc6b3c3e1c06ef415a5
maximecd <37560356+maximecd@users.noreply.github.com>
2022-03-24 13:30:45 +0100

    calcul solde

--- resources/views/compte/index.blade.php
+++ resources/views/compte/index.blade.php
@@ -4,7 +4,7 @@
     <div class="flex flex-nowrap">
         @foreach ($comptes as $compte)
             <a href="{{ route('compte.dashboard', $compte->id) }}">
-                <div class="mr-4 shadow rounded-xl w-[200px] h-[200px] bg-slate-50 p-3 flex flex-col justify-between">
+                <div class="mr-4 shadow rounded-xl w-[200px] h-[200px] bg-slate-50 hover:bg-slate-200 p-3 flex flex-col justify-between">
                     <p class="text-lg font-bold"> {{ $compte->intitule }}</p>
                     <p class="text-cyan-600 self-end">Accéder ➟</p>
                 </div>
@@ -12,7 +12,7 @@
         @endforeach
 
         <a href="{{ route('compte.create') }}">
-            <div class="shadow rounded-xl w-[200px] h-[200px] bg-slate-50 p-3 flex justify-center items-center">
+            <div class="shadow rounded-xl w-[200px] h-[200px] bg-slate-50 hover:bg-slate-200 p-3 flex justify-center items-center">
                 <p class="text-cyan-600 text-5xl">+</p>
             </div>
         </a>
