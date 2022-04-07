3eae415475a4efec766b12bc9a7a3e7c234208b4
maximecd <37560356+maximecd@users.noreply.github.com>
2022-04-07 13:37:33 +0200

    index invite

--- resources/views/compte/index.blade.php
+++ resources/views/compte/index.blade.php
@@ -4,7 +4,14 @@
 
 
 @section('content')
-    <div class="mt-4 flex flex-nowrap">
+
+
+
+    <div>
+        Vos comptes
+    </div>
+
+    <div class="my-6 flex flex-nowrap">
         @foreach ($comptes as $compte)
             <a href="{{ route('compte.dashboard', $compte->id) }}">
                 <div
@@ -22,48 +29,61 @@
             </div>
         </a>
     </div>
-    <hr class="m-8">
 
-    <div class="mt-4 flex flex-nowrap">
-        @foreach ($comptesInv as $compteInv)
-            <a href="{{ route('compte.dashboard', $compteInv->compte->id) }}">
-                <div
-                    class="mr-4 shadow rounded-xl w-[200px] h-[200px] bg-slate-50 hover:bg-slate-200 transition-all p-3 flex flex-col justify-between">
-                    <p class="text-lg font-bold"> {{ $compteInv->compte->intitule }}</p>
-                    <p class="text-cyan-600 self-end">Accéder ➟</p>
-                </div>
-            </a>
-        @endforeach
+    @if(!$comptesInv->isEmpty())
 
-    </div>
+        <div>
+            Vos comptes partagés
+        </div>
 
-    <hr class="m-8">
-    <div class>
-        @foreach ($invitations as $invitation)
-            <div>
+        <div class="my-6 flex flex-nowrap">
+            @foreach ($comptesInv as $compteInv)
+                <a href="{{ route('compte.dashboard', $compteInv->compte->id) }}">
+                    <div
+                        class="mr-4 shadow rounded-xl w-[200px] h-[200px] bg-slate-50 hover:bg-slate-200 transition-all p-3 flex flex-col justify-between">
+                        <p class="text-lg font-bold"> {{ $compteInv->compte->intitule }}</p>
+                        <p class="text-cyan-600 self-end">Accéder ➟</p>
+                    </div>
+                </a>
+            @endforeach
 
+        </div>
+
+    @endif
+
+    @if(!$invitations->isEmpty())
+
+        <div>Invitations</div>
+
+        <div class=class="my-6 flex flex-nowrap">
+            @foreach ($invitations as $invitation)
                 <div>
-                    <p>{{ $invitation->admin_nom }} {{ $invitation->admin_prenom }} vous invite à rejoindre le compte :
-                        {{ $invitation->compte->intitule }}</p>
+
+                    <div>
+                        <p>{{ $invitation->admin_nom }} {{ $invitation->admin_prenom }} vous invite à rejoindre le
+                            compte :
+                            {{ $invitation->compte->intitule }}</p>
 
 
 
-                    <form action="{{ route('invite.accept', $invitation->compte->id) }}" method="post">
-                        @csrf
-                        @method('put')
-                        <button type="submit"
-                            class="bg-green-400 transition hover:bg-green-300 rounded-lg p-3 text-white font-bold">Accepter</button>
-                    </form>
-                    <form action="{{ route('invite.decline', $invitation->compte->id) }}" method="post">
-                        @csrf
-                        @method('delete')
-                        <button type="submit"
-                            class="bg-red-400 transition hover:bg-red-300 rounded-lg p-3 text-white font-bold">Décliner</button>
-                    </form>
+                        <form action="{{ route('invite.accept', $invitation->compte->id) }}" method="post">
+                            @csrf
+                            @method('put')
+                            <button type="submit"
+                                class="bg-green-400 transition hover:bg-green-300 rounded-lg p-3 text-white font-bold">Accepter</button>
+                        </form>
+                        <form action="{{ route('invite.decline', $invitation->compte->id) }}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button type="submit"
+                                class="bg-red-400 transition hover:bg-red-300 rounded-lg p-3 text-white font-bold">Décliner</button>
+                        </form>
+                    </div>
+
                 </div>
+                <hr>
+            @endforeach
+        </div>
+        @endif
 
-            </div>
-            <hr>
-        @endforeach
-    </div>
-@endsection
+    @endsection
