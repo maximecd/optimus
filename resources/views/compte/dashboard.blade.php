52e68e993762146dde4915fc455cadcc72ca3117
maximecd <37560356+maximecd@users.noreply.github.com>
2022-04-07 08:35:18 +0200

    dashboard grid

--- resources/views/compte/dashboard.blade.php
+++ resources/views/compte/dashboard.blade.php
@@ -6,67 +6,72 @@
 @section('content')
 
 
-<div class="dashboard-container h-full p-4">
+    <div class="dashboard-container h-full p-4">
 
-    <h1 class="dashboard-title text-center text-lg mb-10">Tableau de bord - {{ $compte->intitule }}</h1>
-    <div class="historique-transaction bg-gray-50 p-8 shadow-lg rounded-lg overflow-auto h-full">
-        <p class="mb-8 text-lg font-bold">Votre solde : {{ $solde }}€</p>
-
-        <table class="table-auto w-full rounded-lg">
-
-            <tbody>
-                @foreach ($transactions as $transaction)
-                <tr class="border-t">
-                    <td class="{{ $transaction->sens_transaction == 'entrant' ? 'text-green-600' : 'text-red-600' }}">
-                        {{ $transaction->sens_transaction == 'entrant' ? '+' : '-' }}{{ $transaction->montant }}€
-                    </td>
-
-                    <td class="text-right font-semibold" colspan="2">{{ $transaction->intitule }}</td>
+        <div class="solde bg-gray-50 p-8 shadow-lg rounded-lg">
 
 
-                </tr>
-                <tr>
-                    <td>Catégorie</td>
+            <p>Tableau de bord - {{ $compte->intitule }}</p>
+            <p class="mb-8 text-lg font-bold">Votre solde : {{ $solde }}€</p>
+        </div>
+        <div class="historique-transaction bg-gray-50 p-8 shadow-lg rounded-lg overflow-auto">
+            <p>Historique des transactions</p>
+            <table class="table-auto w-full rounded-lg">
 
-                    <td class="text-right"><a class="text-purple-400"
-                            href="{{ route('transaction.show', ['id' => $compte, 'id_transaction' => $transaction]) }}">Consulter</a>
-                    </td>
+                <tbody>
+                    @foreach ($transactions as $transaction)
+                        <tr class="border-t">
+                            <td
+                                class="{{ $transaction->sens_transaction == 'entrant' ? 'text-green-600' : 'text-red-600' }}">
+                                {{ $transaction->sens_transaction == 'entrant' ? '+' : '-' }}{{ $transaction->montant }}€
+                            </td>
 
-
-                </tr>
-                <tr class="border-b">
-                    <td class="text-sm text-gray-500">Le {{ $transaction->date }}</td>
-                    <td class="text-right"><a class="mr-2 text-green-700"
-                            href="{{ route('transaction.edit', ['id' => $compte, 'id_transaction' => $transaction]) }}">
-                            Modifier
-                        </a>
-                        <a class="text-red-500"
-                            href="{{ route('transaction.destroy', ['id' => $compte->id, 'id_transaction' => $transaction]) }}">
-                            Supprimer
-                        </a>
-
-                    </td>
-                </tr>
-                @endforeach
-                @if (count($transactions) == 0)
-                <tr>
-                    <td colspan="5" class=""><a class="text-purple-400 underline"
-                            href="{{ route('transaction.create', $compte->id) }}">Aucune transaction. Créez-en
-                            !</a></td>
-                </tr>
-                @endif
-            </tbody>
-        </table>
+                            <td class="text-right font-semibold" colspan="2">{{ $transaction->intitule }}</td>
 
 
+                        </tr>
+                        <tr>
+                            <td>Catégorie</td>
 
+                            <td class="text-right"><a class="text-purple-400"
+                                    href="{{ route('transaction.show', ['id' => $compte, 'id_transaction' => $transaction]) }}">Consulter</a>
+                            </td>
+
+
+                        </tr>
+                        <tr class="border-b">
+                            <td class="text-sm text-gray-500">Le {{ $transaction->date }}</td>
+                            <td class="text-right"><a class="mr-2 text-green-700"
+                                    href="{{ route('transaction.edit', ['id' => $compte, 'id_transaction' => $transaction]) }}">
+                                    Modifier
+                                </a>
+                                <a class="text-red-500"
+                                    href="{{ route('transaction.destroy', ['id' => $compte->id, 'id_transaction' => $transaction]) }}">
+                                    Supprimer
+                                </a>
+
+                            </td>
+                        </tr>
+                    @endforeach
+                    @if (count($transactions) == 0)
+                        <tr>
+                            <td colspan="5" class=""><a class="text-purple-400 underline"
+                                    href="{{ route('transaction.create', $compte->id) }}">Aucune transaction. Créez-en
+                                    !</a></td>
+                        </tr>
+                    @endif
+                </tbody>
+            </table>
+
+
+
+        </div>
+        <div class="ajouter-transaction bg-gray-50 p-8 shadow-lg rounded-lg">
+            <a href="{{ route('transaction.create', $compte->id) }}">Ajouter</a>
+        </div>
+        <div class="settings bg-gray-50 p-8 shadow-lg rounded-lg">
+            <a href="{{ route('compte.edit', $compte->id) }}">Settings</a>
+        </div>
     </div>
-    <div class="ajouter-transaction bg-gray-50 p-8 shadow-lg rounded-lg">
-        <a href="{{ route('transaction.create', $compte->id) }}">Ajouter</a>
-    </div>
-    <div class="settings bg-gray-50 p-8 shadow-lg rounded-lg">
-        <a href="{{ route('compte.edit', $compte->id) }}">Settings</a>
-    </div>
-</div>
 
 @endsection
