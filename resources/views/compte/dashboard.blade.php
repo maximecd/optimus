cc95a1738288e75b8102dcae4b4673e8e1be11ba
wayry <wayrygaming@gmail.com>
2022-04-07 13:03:36 +0200

    tips and solde

--- resources/views/compte/dashboard.blade.php
+++ resources/views/compte/dashboard.blade.php
@@ -1,21 +1,35 @@
 @extends('layouts.app')
 @section('title', 'Dashboard')
 @section('content')
-<div class="h-full ">
-    <div class="pl-4">
-        <a href="{{ route('compte.index', $compte->id) }}">
-            <div class="arrow"><span>&larr;</span> Retour</div>
-        </a>
-    </div>
-    <div class="dashboard-container h-full p-4">
+<div class="h-full">
 
-        <div class="solde bg-gray-50 p-8 shadow-lg rounded-lg ">
+    <div class="dashboard-container h-full p-4">
+        <div class="retour">
+            <a href="{{ route('compte.index') }}">
+                <div class="arrow"><span>&larr;</span> Retour</div>
+            </a>
+        </div>
+        <div class="solde bg-gray-50 p-8 shadow-lg rounded-lg relative ">
             <p class="font-bold text-xl mb-4">Bienvenue {{ $user->name }} !</p>
             <hr>
             <br>
             <p>Vous êtes sur le tableau de bord du compte : <span class="font-bold">{{ $compte->intitule }}</span></p>
             <br>
-            <p class="mb-8">Votre solde est de {{ $solde }}€</p>
+            <div class="mb-8 absolute bottom-0">
+                <p class=" text-xl">Solde : <span
+                        class="font-bold text-2xl {{ $solde > 0 ? 'text-green-600' : 'text-red-600'}}"> {{ $solde }}
+                        €</span></p>
+                <p class="tips italic">
+                    @if ($solde <= 50) 
+                    Vous devriez faire attention à vos dépenses ! 
+                    @elseif ($solde >= 500 && $solde <1200)
+                    Continuez ainsi vous êtes dans le droit chemin !
+                    @elseif ($solde >= 1200)
+                    Essayez d'investir ou d'épargnez !
+                    @endif
+                </p>
+            </div>
+
         </div>
         <div class="historique-transaction bg-gray-50 p-8 shadow-lg rounded-lg overflow-auto">
             <p class="text-xl font-bold mb-4">Historique des transactions</p>
@@ -23,7 +37,7 @@
 
                 <tbody>
                     @foreach ($transactions as $transaction)
-                    <tr class="border-t">
+                    <tr class="border-t ">
                         <td
                             class="{{ $transaction->sens_transaction == 'entrant' ? 'text-green-600' : 'text-red-600' }}">
                             {{ $transaction->sens_transaction == 'entrant' ? '+' : '-' }}{{ $transaction->montant }}€
