6b9f8a4f20d1967acda3fc07cab4f98063f73472
Jérémy PION <77725625+jrmy17@users.noreply.github.com>
2022-04-07 09:08:05 +0200

    Update edit.blade.php

--- resources/views/transaction/edit.blade.php
+++ resources/views/transaction/edit.blade.php
@@ -17,7 +17,6 @@
                         </a>
                     </div>
 
-
                     <div class="bg-gray-200 flex space-between items-center p-5 rounded-lg w-full">
                         <div class="w-1/3">
                             <label for="intitule">Intitule de la transaction</label>
