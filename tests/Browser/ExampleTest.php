eb24517f760cca631fc7b01c042dd729f6cb93e4
wayry <wayrygaming@gmail.com>
2022-04-11 15:21:53 +0200

    exempletest

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -17,15 +17,11 @@ class ExampleTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-<<<<<<< HEAD
-                    ->assertSee('Laravel');
-=======
                     ->assertSee('Email')
                     ->type('email','admin@optimus.fr')
                     ->type('password','password')
                     ->press('SE CONNECTER')
                     ->assertSee('Vos comptes');
->>>>>>> bc5c4c4214884067cf0457bb6db3ae8ffadb63db
         });
     }
 }
